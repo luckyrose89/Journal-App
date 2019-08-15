3e8013578fdce8a5df3be1cc216b0baeabfdf52c
avidrutham <divyamathur89@gmail.com>
2019-08-15 18:30:02 +0530

    remove passport dependencies

--- controllers/userControl.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-const Note = require("../models/user");
--- models/user.js
+++ models/user.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-var bcrypt = require("bcrypt");
-const uniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema();
 
 const userSchema = new Schema({
-  email: {
+  username: {
     type: String,
     required: true,
     unique: true
@@ -13,16 +11,6 @@ const userSchema = new Schema({
     type: String,
     required: true
   }
-});
-
-userSchema.plugin(uniqueValidator);
-
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.passwordHash);
-};
-
-userSchema.virtual("password").set(function(value) {
-  this.passwordHash = bcrypt.hashSync(value, 12);
 });
 
 module.exports = mongoose.model("User", userSchema);
