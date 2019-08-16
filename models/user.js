352d23094269fee3b3d6de7045fd50d8da700fcc
avidrutham <divyamathur89@gmail.com>
2019-08-16 16:52:10 +0530

    add bcrypt password hashing methods to user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -12,5 +13,25 @@ const userSchema = new Schema({
     required: true
   }
 });
+
+// Hash password before saving user document
+
+userSchema.pre("save", function(next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+// Compare and match passwords
+userSchema.methods.checkPassword = function(passwordAttempt, callback) {
+  bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
