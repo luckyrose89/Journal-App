778bd806267d5cf543b5c46ab29d7f4d01786001
avidrutham <divyamathur89@gmail.com>
2019-08-24 14:16:11 +0530

    complete noteform styles

--- client/src/notes/noteForm.js
+++ client/src/notes/noteForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, TextField, Button, Typography } from "@material-ui/core";
 
 class NoteForm extends React.Component {
   constructor() {
@@ -40,28 +41,57 @@ class NoteForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <Container maxWidth="lg">
+        <Typography
+          className="titlePadding"
+          align="left"
+          style={{ marginBottom: 15 }}
+          variant="h6"
+        >
+          New Entry | Create a Note
+        </Typography>
         <form onSubmit={this.handleSubmit}>
-          <h4>New Journal Entry</h4>
-          <label>Title</label>
-          <input
+          <TextField
+            id="outlined-full-width"
+            label="Title"
+            style={{ margin: 8, marginBottom: 15 }}
             name="title"
             value={this.state.title}
             onChange={this.handleChange}
             type="text"
             placeholder="title"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true
+            }}
           />
-          <label>Body</label>
-          <input
+          <TextField
+            id="outlined-full-width"
+            label="Entry"
+            style={{ margin: 8, marginBottom: 15 }}
             name="body"
             value={this.state.body}
             onChange={this.handleChange}
-            type="text"
             placeholder="Enter text"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true
+            }}
           />
-          <button>Submit</button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ marginLeft: 8 }}
+          >
+            Submit
+          </Button>
         </form>
-      </div>
+      </Container>
     );
   }
 }
